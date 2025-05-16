b41b265d7b17d713e58e39dab03676c059bdf208
ujjainim <ujjainimukherjee97@gmail.com>
2025-05-16 23:59:26 +0530

    Annotation

--- src/main/java/com/cg/onlinefooddeliverysystem/service/RoleCheck.java
+++ src/main/java/com/cg/onlinefooddeliverysystem/service/RoleCheck.java
@@ -1,5 +1,12 @@
 package com.cg.onlinefooddeliverysystem.service;
 
-public @interface RoleCheck {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@interface RoleCheck {
+	String role();
 }
