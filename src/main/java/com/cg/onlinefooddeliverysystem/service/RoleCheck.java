ad74ca867d837693a34b065d4e9bbfc788226d53
ujjainim <ujjainimukherjee97@gmail.com>
2025-05-17 00:02:19 +0530

    Annotation

--- src/main/java/com/cg/onlinefooddeliverysystem/service/RoleCheck.java
+++ src/main/java/com/cg/onlinefooddeliverysystem/service/RoleCheck.java
@@ -4,9 +4,19 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+/**
+ * The {@code RoleCheck} annotation is used to specify a required role for a class.
+ * This annotation can be applied to any type (class, interface, enum) at runtime,
+ * and it is typically used for role-based security checks within the application.
+ */
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @interface RoleCheck {
+	/**
+     * Specifies the required role for the annotated class.
+     *
+     * @return the name of the required role
+     */
 	String role();
 }
