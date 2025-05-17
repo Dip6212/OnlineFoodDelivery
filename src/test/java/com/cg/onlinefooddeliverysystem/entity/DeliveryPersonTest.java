eb866dcd42427dd9d2930b888002567c73062b79
Dipan Mukherjee <mukherjeedip123@gmail.com>
2025-05-17 09:56:05 +0530

    test

---
+++ src/test/java/com/cg/onlinefooddeliverysystem/entity/DeliveryPersonTest.java
@@ -0,0 +1,14 @@
+package com.cg.onlinefooddeliverysystem.entity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DeliveryPersonTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
