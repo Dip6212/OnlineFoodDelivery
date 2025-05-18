cfa65488d90d6513381feb40040573980610146a
Dipan Mukherjee <mukherjeedip123@gmail.com>
2025-05-18 19:34:08 +0530

    entity

--- src/main/java/com/cg/onlinefooddeliverysystem/entity/User.java
+++ src/main/java/com/cg/onlinefooddeliverysystem/entity/User.java
@@ -2,20 +2,19 @@
 
 package com.cg.onlinefooddeliverysystem.entity;
 /**
- * This abstract class defines a generic User entity in the Online Food Delivery System.
- * It contains common fields like 'id' and 'name', along with their getter and setter methods.
- * The class also declares an abstract method 'showProfile()', which must be implemented by subclasses.
- * This design allows for flexible and reusable user-related functionality across different user roles.
+*This abstract class declares a User in the Online Food Delivery System.
+* It has fields such as 'id' and 'name' and their setter and getter.
+* It defines an abstract method 'showProfile()', which needs to be overridden by subclasses.
  * @author Debanjana Bag
  * @since 1.0
  */
 
 public abstract class User {
 
-	// Unique identifier for the user
+	// Unique identifier for user
 	protected String id;
 
-	// Name of the user
+	// Name of user
 	protected String name;
 
 	public User() {
@@ -23,26 +22,42 @@ public abstract class User {
 		setName(name);
 	}
 
-	// Returns the user's ID
+	
+	/**
+     * Returns the user's ID
+     * @return the user's ID as a String
+     */
 	public String getId() {
 		return id;
 	}
 
-	// Sets the user's ID
+	
+	/**
+     * Sets the user's ID
+     * @param id the unique identifier for the user
+     */
 	public void setId(String id) {
 		this.id = id;
 	}
 
-	// Returns the user's name
+	
+	 /**
+     * Returns the user's name
+     * @return the user's name as a String
+     */
 	public String getName() {
 		return name;
 	}
 
-	// Sets the user's name
+	
+	/**
+     * Sets the user's name
+     * @param name of the user
+     */
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	// Abstract method to display the user's profile; must be implemented by subclasses
+	// Abstract method to display the user's profile
 	public abstract void showProfile();
 }
